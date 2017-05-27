64777d74c1a3db7add72985ddad573b512a97ff4
Aaron-zheng <fucheng.zheng@gmail.com>
2017-05-27 11:10:19 +0800

    从springmvc换成是springboot的shiro

---
+++ src/main/java/com/sightcorner/shiro/config/ShiroConfiguration.java
@@ -0,0 +1,15 @@
+package com.sightcorner.shiro.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Configuration
+public class ShiroConfiguration {
+
+    private static Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+
+    
+
+}
